ddcefadc07467b30e0f5f7b00351ce5870abc005
Adam Cihlar <adam.cihlar3@gmail.com>
2021-09-04 22:15:57 +0200

    New feature that is not working

--- gitit.py
+++ gitit.py
@@ -3,5 +3,9 @@ import pandas as pd
 import sys
 
 df = pd.DataFrame({"a":[1,2,3,4], "b":[3,4,5,6]})
+try:
+    df = pd.DataFrame + sys.argv[2]
+except:
+    pass
 if sys.argv[1]=='print':
     print(df)
