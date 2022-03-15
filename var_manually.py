3335543abc38eaaba403c5a709b9a94cd3f6e663
adamcihlar <adam.cihlar3@gmail.com>
2022-03-15 12:13:27 +0100

    Add estimate function

--- var_manually.py
+++ var_manually.py
@@ -11,6 +11,7 @@ def generate_data(shape=(100, 2), with_bias=True):
 
 
 def expand_data(data, order=1, bias=True):
+    shp = data.shape
     for i in range(data.shape[1]):
         for j in range(1, order+1):
             data = np.column_stack([
@@ -19,13 +20,33 @@ def expand_data(data, order=1, bias=True):
                        constant_values=(np.nan))
             ])
 
-#     if with_bias:
-#         ones = np.ones(shape[0])
-#         data = np.column_stack([ones, ones, data])
+    data = data[~np.isnan(data).any(axis=1)]
+    y = data[:, 0:shp[1]]
+    x = data[:, shp[1]:]
 
-    return data
+    if bias:
+        ones = np.ones((x.shape[0], shp[1]))
+        x = np.column_stack([ones, x])
+
+    return x, y
+
+
+def rmse(y_true, y_hat):
+    return (np.mean((y_hat - y_true)**2))**(1/2)
+
+
+def estimate_var(x, y, lr=0.0001, n_iter=1000):
+    beta = np.random.standard_normal((y.shape[1], x.shape[1]))
+    for i in range(n_iter):
+        grad = (beta.dot(x.T) - y.T).dot(x)
+        beta -= (lr * grad)
+        if (i % 20 == 0):
+            print(rmse(y, x.dot(beta.T)))
+    return beta
 
 if __name__=='__main__':
 
     data = generate_data()
-    data = expand_data(data, order=2)
+    x, y = expand_data(data, order=1)
+
+    beta = estimate_var(x, y)
